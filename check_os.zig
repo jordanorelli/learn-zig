1d134ae04b77e2d85e3e719d09c20075eccc1e94
Jordan Orelli <jordanorelli@gmail.com>
2020-12-24 21:42:31 +0000

    check_os simplification

--- check_os.zig
+++ check_os.zig
@@ -2,15 +2,10 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 pub fn main() !void {
-    switch (builtin.os.tag) {
-        .windows => {
-            std.debug.print("hello windows OS: {}\n", .{builtin.os.tag});
-        },
-        .linux => {
-            std.debug.print("hello linux-based OS: {}\n", .{builtin.os.tag});
-        },
-        else => {
-            std.debug.print("hello mystery OS: {}\n", .{builtin.os.tag});
-        },
-    }
+    const name = switch (builtin.os.tag) {
+        .windows => "windows",
+        .linux => "linux",
+        else => "mystery",
+    };
+    std.debug.print("hello {}\n", .{name});
 }
